e40ec83156eb32e9730870bd7044cc492c64255e
Hussi503 <86091094+Hussi503@users.noreply.github.com>
2025-09-24 23:08:11 +0530

    Update build.sh

--- Bash/build.sh
+++ Bash/build.sh
@@ -6,5 +6,5 @@ cd gold/
 sudo git clone https://github.com/Hussi503/Day6.git
 cd Day6/Code
 sudo docker build -t react-microk8s -f golddockerfile .
-sudo docker tag react-nginx-ansible:latest hussain503/react-nginx-ansible:latest ##make sure you did docker login
-sudo docker push hussain503/react-nginx-ansible:latest
+sudo docker tag react-nginx-ansible:latest hussi503/sagar-k-practice:latest ##make sure you did docker login
+sudo docker push hussi503/sagar-k-practice:latest
